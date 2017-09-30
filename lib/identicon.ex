657013e51c813245288277662aa5cb55784f4ea0
franknfjr <frankdonascimentojr@gmail.com>
2017-09-29 23:57:48 -0300

    pattern matching structs

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,13 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail] } = image
+
+    [r, g, b]
   end
 
   def hash_input(input) do
