498e00f2452a1131c55f7dd053e1a45b61bb8ffe
franknfjr <frankdonascimentojr@gmail.com>
2017-09-30 10:34:52 -0300

    build pixel map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,11 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
