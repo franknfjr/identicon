00f5f8136e82ac7ef2628669307c49a33b61725d
franknfjr <frankdonascimentojr@gmail.com>
2017-09-30 10:55:02 -0300

    creating the pixel map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,7 +9,17 @@ defmodule Identicon do
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
 
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
